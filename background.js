17b78fafb286e4463a99eb15281cea748954b7d5
Ethan Seulin <ethan.seulin@gmail.com>
2026-01-22 22:11:56 +0100

    Scrap des données, formatage, début de la connexion WS, hook sur les différents éléments HTML, debouncer, communication entre content script et service worker script.......

--- background.js
+++ background.js
@@ -1,3 +1,44 @@
-chrome.runtime.onMessage.addListener((msg) => {
-  console.log("🎧 Chanson actuelle :", msg.artist, "-", msg.title);
+let url = "ws://localhost:3000";
+let websocket = new WebSocket(url);
+
+let intervalReconnect;
+
+websocket.addEventListener("open", () => {
+    console.log("CONNECTED!");
+})
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (websocket.readyState === websocket.OPEN) {
+        websocket.send(message);
+        console.log("message sent");
+    }
+});
+
+websocket.addEventListener("close", () => {
+    intervalReconnect = setInterval(connect, 5000);
+})
+
+const connect = () => {
+    if (websocket.readyState === websocket.OPEN) {
+        console.log("BACK ONLINE");
+        clearInterval(intervalReconnect);
+        return;
+    }
+    console.log("Connecting...");
+    websocket = new WebSocket(url);
+}
+
+chrome.runtime.onConnect.addListener(function (port) {
+    if (port.name !== "trackinfo") return;
+
+
+    port.onMessage.addListener(function (msg) {
+        if (websocket.readyState !== websocket.OPEN) {
+            console.log("WebSocket hors ligne impossible d'envoyer les données.");
+            return;
+        }
+        websocket.send(msg.data);
+        console.log("Message received:\n" + msg.data.currentTime);
+    });
 });
--- scscrobbler.js
+++ scscrobbler.js
@@ -1,10 +1,100 @@
-function sendTrackInfo() {
-  const title = document.querySelector('.playbackSoundBadge__titleLink')?.innerText.trim();
-  const artist = document.querySelector('.playbackSoundBadge__lightLink')?.innerText.trim();
+/** ENVOI DES INFOS QUAND
+ * — le nom de la musique change
+ * — le bouton play est changé
+ */
+const port = chrome.runtime.connect({name: "trackinfo"});
+let debouncer;
 
-  if (title && artist) {
-    chrome.runtime.sendMessage({ artist, title });
-  }
+
+/**
+ * Return the title, author, and avatar url of the song.
+ * @return {{title: string, author: string, avatar: string}}
+ */
+const getBadgeData = () => {
+    const titleElement = document.querySelector(".playbackSoundBadge__titleLink").lastElementChild;
+    const authorElement = document.querySelector(".playbackSoundBadge__lightLink");
+    const avatarElement = document.querySelector(".playbackSoundBadge__avatar");
+
+    const avatarUrl = avatarElement.querySelector("div > span")
+        .style.backgroundImage
+        .replace('url("', "")
+        .replace('")', "");
+
+    return { title:titleElement.innerText, author:authorElement.innerText, avatar:avatarUrl }
 }
 
-setInterval(sendTrackInfo, 2000);
+/**
+ * Return the state of the music
+ * @return {string} "PAUSED" or "PLAYING"
+ */
+const getState = () => {
+    const playButton = document.querySelector(".playControl");
+    const state = playButton.querySelector("div > span");
+    if(state.textContent === "Pause current") {
+        return "PLAYING";
+    }
+    return "PAUSED";
+};
+
+/**
+ * Return the current time progress of the song.
+ * @return {number} Current time progress in `seconds`.
+ */
+const getCurrentTime = () => {
+    const timeElement = document.querySelector(".playbackTimeline__timePassed");
+    const timeString = timeElement.lastElementChild.textContent;
+    return timeStringToSeconds(timeString);
+};
+
+/**
+ * Return the duration of the track.
+ * @return {number} `seconds` of the track.
+ */
+const getTrackDuration = () => {
+    const durationElement = document.querySelector(".playbackTimeline__duration");
+    const durationString = durationElement.lastElementChild.textContent;
+    return timeStringToSeconds(durationString);
+}
+
+/**
+ * Convert String of time in seconds
+ * @param timeString {String}
+ * @return {number}
+ */
+const timeStringToSeconds = timeString => {
+    let timeTab = timeString.trim().split(":").map(Number); // [ 4, 27 ]
+    let totalSeconds = 0;
+
+    for(let i = 0; i<timeTab.length-1; i++) {
+        totalSeconds += timeTab[i] * Math.pow(60, (timeTab.length - 1 - i));
+    }
+
+    totalSeconds += timeTab[timeTab.length-1];
+    return totalSeconds;
+}
+
+
+/**
+ * Format data and send it to the service worker script.
+ */
+const sendChanges = () => {
+    const data = {
+        track: getBadgeData(),
+        currentTime: getCurrentTime(),
+        trackDuration: getTrackDuration(),
+        state: getState()
+    }
+    const formatData = JSON.stringify(data);
+    console.log(data);
+    port.postMessage({ data: data});
+}
+
+
+const temoinHook = new MutationObserver((mutations) => {
+    console.log("changements détectés !");
+    clearTimeout(debouncer);
+    debouncer = setTimeout(sendChanges, 300);
+});
+
+temoinHook.observe(document.querySelector(".playbackSoundBadge"), { childList: true, subtree:true });
+temoinHook.observe(document.querySelector(".playControl"), { childList: true, subtree: true });
